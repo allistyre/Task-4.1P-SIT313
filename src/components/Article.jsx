f077948707ba9dc6e05bc1cdf9236babc2c80ab1
Allistyre <allistyre@gmail.com>
2025-07-22 15:02:27 +1000

    uncaught runtime npm error

---
+++ src/components/Article.jsx
@@ -0,0 +1,21 @@
+import {Card, Image, CardContent, CardHeader, CardDescription, Rating} from 'semantic-ui-react';
+
+
+function Article({title, description, img, rating}) {
+    return (
+        <Card>
+        <Image src={img} />
+        <CardContent className="card-content">
+            <CardHeader>{title}</CardHeader>
+            <CardDescription>
+                {description}
+            </CardDescription>
+        </CardContent>
+        <CardContent extra>
+            <Rating icon='star' defaultRating={rating} maxRating={5}/>
+        </CardContent>
+    </Card>
+    );
+}
+
+export default Article;
