5b39d3d7eebd1eb5d69752a6ab4dc2e2f0046adc
Allistyre <allistyre@gmail.com>
2025-07-22 14:26:43 +1000

    before using arrays and map function on cardssection

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,7 +1,45 @@
 import './css/CardsSection.css';
 import {Card, CardContent, CardDescription, CardHeader, Image, Rating, CardMeta, Icon, CardGroup} from 'semantic-ui-react';
+import {useState} from "react";
 
 function CardsSection() {
+    const [articles , setArticles] = useState(
+        [
+            {
+                title: 'How to use React',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n' +
+                    'Architecto consectetur harum, placeat qui rem repellendus saepe.\n' +
+                    'A aliquam aspernatur, at eligendi eum fugit id illum iusto natus\n' +
+                    'nulla quidem, recusandae saepe totam. Architecto atque, dicta ex\n' +
+                    'expedita fugit hic labore, maiores perspiciatis quam quasi repellat\n' +
+                    'saepe soluta totam voluptate voluptatibus.',
+                img: 'https://placehold.co/400x200',
+                rating: 4
+            },
+            {
+                title: 'Beginners guide to HTML + CSS',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n' +
+                    'Architecto consectetur harum, placeat qui rem repellendus saepe.\n' +
+                    'A aliquam aspernatur, at eligendi eum fugit id illum iusto natus\n' +
+                    'nulla quidem, recusandae saepe totam. Architecto atque, dicta ex\n' +
+                    'expedita fugit hic labore, maiores perspiciatis quam quasi repellat\n' +
+                    'saepe soluta totam voluptate voluptatibus.',
+                img: 'https://placehold.co/400x200',
+                rating: 3
+            },
+            {
+                title: 'Why JS is crucial for web development',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n' +
+                    'Architecto consectetur harum, placeat qui rem repellendus saepe.\n' +
+                    'A aliquam aspernatur, at eligendi eum fugit id illum iusto natus\n' +
+                    'nulla quidem, recusandae saepe totam. Architecto atque, dicta ex\n' +
+                    'expedita fugit hic labore, maiores perspiciatis quam quasi repellat\n' +
+                    'saepe soluta totam voluptate voluptatibus.',
+                img: 'https://placehold.co/400x200',
+                rating: 5
+            }
+        ]
+    );
     return (
         <div className="cards__section-container">
             <div className="cards__section-header">
